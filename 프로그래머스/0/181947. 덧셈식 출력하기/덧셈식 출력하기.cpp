e7efacbf779a09627679cb8d6f8a1f86a9681ffb
Ju100 <kuberin_b@naver.com>
2025-07-18 05:48:27 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 2.04 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    ostream& os(cout);
+    os << a << " + " << b << " = " << a+b <<endl;
+    return 0;
+}
