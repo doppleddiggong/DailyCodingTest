18a3e4c0361c8defc610aa4e5e53e0016a64e760
Ju100 <kuberin_b@naver.com>
2025-07-20 09:40:45 +0900

    [level 2] Title: 주식가격, Time: 3.63 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.Length];
+        Array.Clear(answer, 0, answer.Length);
+
+        for(int i=0; i<prices.Length; i++) {
+            for(int j=i+1; j<prices.Length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j])
+                    break;
+            }
+        }
+        return answer;
+    }
+}
