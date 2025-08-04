543ebfcb426ed1fe1b1ef7a868e5178f14eb00c2
Ju100 <kuberin_b@naver.com>
2025-08-04 12:51:34 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+     int answer = 0;
+
+     while (a != b)
+     {
+        a = ceil((float)a / 2);
+        b = ceil((float)b / 2);
+
+        answer++;
+     }
+
+     return answer;
+}
