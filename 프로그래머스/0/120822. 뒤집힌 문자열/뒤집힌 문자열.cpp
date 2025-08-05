e3e179c4b6e0a630330f988769f0cade1f456e94
Ju100 <kuberin_b@naver.com>
2025-08-06 08:05:43 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+    std::reverse(my_string.begin(), my_string.end() );
+    return my_string;
+}
