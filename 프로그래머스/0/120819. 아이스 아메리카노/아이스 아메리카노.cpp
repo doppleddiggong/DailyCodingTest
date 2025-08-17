b0c3cf2ed445022cb571f9586a92cf509ac69d01
Ju100 <kuberin_b@naver.com>
2025-08-17 21:25:37 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int money) {
+    vector<int> answer;
+    
+    
+    
+    
+    return { money / 5500, money % 5500 };
+}
