a6f61e514938fc8f5e2d53f12d8c734e764a01b9
Ju100 <kuberin_b@naver.com>
2025-08-28 20:56:36 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array) {
+    vector<int> answer;
+    answer.push_back(0);
+    answer.push_back(0);
+    for (int i = 0; i < array.size(); i++)
+    {
+        if (array[i] > answer[0])
+        {
+            answer[0] = array[i];
+            answer[1] = i;
+        }
+    }
+
+    return answer;
+}
