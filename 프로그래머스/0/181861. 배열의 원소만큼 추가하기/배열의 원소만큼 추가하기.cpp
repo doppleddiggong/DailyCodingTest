818722245acd76de470b4c63e89cb678de738535
Ju100 <kuberin_b@naver.com>
2025-09-08 08:48:29 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 2.24 ms, Memory: 5.54 MB -BaekjoonHub

---
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    for(auto n : arr)
+    {
+        for (int i = 0; i < n; i++)
+            answer.push_back(n);
+    }
+    return answer;
+
+}
