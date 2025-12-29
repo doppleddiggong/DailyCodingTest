83b9d05fa966fdcc622777ac27f93c9aa9fc5b82
Ju100 <ju100.bae@gmail.com>
2025-12-29 20:35:04 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << R"(\    /\)" <<std::endl;
+    std::cout << R"( )  ( '))" <<std::endl;
+    std::cout << R"((  /  ))" <<std::endl;
+    std::cout << R"( \(__)|)" <<std::endl;
+}
