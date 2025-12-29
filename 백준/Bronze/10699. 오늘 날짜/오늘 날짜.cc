576ffc4facba0247fca9063b16355c22e15ddab8
Ju100 <ju100.bae@gmail.com>
2025-12-29 20:21:28 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+int main()
+{
+    std::time_t t = std::time(nullptr);
+    std::tm tm = *std::localtime(&t);
+    
+    std::cout << std::put_time(&tm, "%Y-%m-%d") << std::endl;
+}
