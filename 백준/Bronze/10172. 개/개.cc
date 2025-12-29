aed49193042b5cac8d2d62ba41988fba90f64b29
Ju100 <ju100.bae@gmail.com>
2025-12-29 20:40:18 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+   
+    std::cout << R"dog(|\_/|)dog" << std::endl;
+    std::cout << R"dog(|q p|   /})dog" << std::endl;
+    std::cout << R"dog(( 0 )"""\)dog" << std::endl;
+    std::cout << R"dog(|"^"`    |)dog" << std::endl;
+    std::cout << R"dog(||_/=\\__|)dog" << std::endl;
+    return 0;
+    
+}
