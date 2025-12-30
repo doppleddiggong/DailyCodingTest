c3bf2c6527949a043a3e54b280b4c6bb331e1f48
Ju100 <ju100.bae@gmail.com>
2025-12-30 22:10:35 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a,b;
+    
+    cin >> a >> b;
+    double f = a/b;
+    cout.precision(10);
+    cout << f << endl;
+    return 0;
+}
