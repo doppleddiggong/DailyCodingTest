94fc79e6f44b0b93b64f15aa66c78dddff92374c
Ju100 <ju100.bae@gmail.com>
2026-01-01 15:02:12 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long price;
+
+    cin >> price;
+
+    cout << (long long)(price * 0.78) << " " << (long long)(price * 0.956) << endl;
+    return 0;
+}
