c01e6ab20c15a0ce8435203bc5b3d2b9e83e1aa0
Ju100 <ju100.bae@gmail.com>
2026-01-03 16:35:23 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -2,24 +2,28 @@
 #include <vector>
 #include <algorithm>
 
+
+using namespace std;
+
 int main()
 {
-    int N = 0;
-    std::cin >> N;
-
-    // 1. N개의 숫자를 받아야한다
-    // 2. N개의 숫자를 Array로 받아야한다
-    std::vector<int> NumSpace(N);
-
-    for (int i = 0; i < N; i++)
-        std::cin >> NumSpace[i];
-
-    std::sort(NumSpace.begin(), NumSpace.end());
-
-    for( auto n : NumSpace)
+    int N;
+    cin >> N;
+    
+    vector<int> data;
+    for(int i = 0; i < N; i++ )
     {
-        std::cout << n <<  std::endl;
+        int V;
+        cin >> V;
+        
+        data.push_back(V);
     }
     
+    sort(data.begin(), data.end());
+    
+    for(auto c : data )
+    {
+        cout << c << '\n';
+    }
     return 0;
 }
