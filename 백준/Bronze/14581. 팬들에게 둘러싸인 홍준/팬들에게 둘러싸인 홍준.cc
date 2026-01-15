be606d2375f3528545b1527e8e556039d0c06d11
Ju100 <ju100.bae@gmail.com>
2026-01-15 09:36:52 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string value;
+    
+    cin >> value;
+    
+    
+    cout<< ":fan::fan::fan:" << '\n';
+    cout<< ":fan::"<<value<<"::fan:" << '\n';
+    cout<< ":fan::fan::fan:" << '\n';
+    
+    
+    
+}
