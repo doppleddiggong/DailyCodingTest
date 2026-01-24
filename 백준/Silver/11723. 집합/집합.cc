d38c9bc29ad115a4d5887ef51b1079b9c0d8211f
Ju100 <ju100.bae@gmail.com>
2026-01-24 17:06:01 +0900

    [Silver V] Title: 집합, Time: 820 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <set>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    unordered_map<int, bool> data;
+
+    int N;
+    cin >> N;
+
+    string command;
+    int value;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> command;
+
+        if (command == "add")
+        {
+            cin >> value;
+
+            data[value] = true;
+        }
+        else if (command == "remove")
+        {
+            cin >> value;
+            data[value] = false;
+        }
+        else if (command == "check")
+        {
+            cin >> value;
+            if (data[value])
+            {
+                cout << "1" << '\n';
+            }
+            else
+            {
+                cout << "0" << '\n';
+            }
+        }
+        else if (command == "toggle")
+        {
+            cin >> value;
+            data[value] = !data[value];
+        }
+        else if (command == "all")
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                data[i] = true;
+            }
+        }
+        else if (command == "empty")
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                data[i] = false;
+            }
+        }
+    }
+
+    return 0;
+}
